f1647da058896fe051edb2e065194ce66a734e28
Suhaib <suhaib1602@gmail.com>
2022-01-01 16:24:55 +0000

    Implemented data file for database within Context/GuestContext.cs

--- Backend/BookingSystem/BookingSystem/Context/GuestContext.cs
+++ Backend/BookingSystem/BookingSystem/Context/GuestContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BookingSystem.Context
+﻿using Microsoft.EntityFrameworkCore;
+
+using BookingSystem.Models;
+
+namespace BookingSystem.Context
 {
-    public class GuestContext
+    public class GuestContext: DbContext
     {
+        public GuestContext(DbContextOptions<GuestContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<Guest> Guests { get; set; }
     }
 }
