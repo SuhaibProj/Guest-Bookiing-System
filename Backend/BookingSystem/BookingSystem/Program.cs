051c3353261668a749b34d0487264416a7a59d1b
Suhaib <suhaib1602@gmail.com>
2022-01-01 16:06:18 +0000

    Added Cross-Origin-Scripting within Program.cs

--- Backend/BookingSystem/BookingSystem/Program.cs
+++ Backend/BookingSystem/BookingSystem/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add CORS
+builder.Services.AddCors(options => {
+    options.AddPolicy("CORSPolicy", builder => {
+        builder
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CORSPolicy");
 
 app.UseHttpsRedirection();
 
